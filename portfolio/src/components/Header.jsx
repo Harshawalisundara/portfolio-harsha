316f19aa5336cbdbf72d70775ca738908fe9abe9
Harsha walisundara <harshawalisundara8@gmail.com>
2024-11-19 09:50:05 +0530

    change scroll bar
    change scroll bar

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,22 @@
+/**
+ * @copyright 2024 harshawalisundara
+ * @license Apache-2.0
+ */
+
+/**
+ * 
+ * @returns 
+ */
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900/0">
+        <div className=""></div>
+
+
+
+    </header>
+  )
+}
+
+export default Header
